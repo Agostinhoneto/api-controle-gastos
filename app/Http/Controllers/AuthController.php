8bb2c9f3d10b73825847a5c58949f6c6b0588608
Agostinhoneto <agostneto6@gmail.com>
2024-07-30 17:46:46 -0300

    primeiros arquivos

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,32 +4,30 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
-    
     public function login(Request $request)
     {
-        $request->validate([
-            'email' => 'required|string|email',
-            'password' => 'required|string',
-            'device_name' => 'required|string',
-        ]);
-
         $user = User::where('email', $request->email)->first();
-
         if (! $user || ! Hash::check($request->password, $user->password)) {
+            dd($user);    
             throw ValidationException::withMessages([
                 'email' => ['The provided credentials are incorrect.'],
             ]);
         }
-        $token = $user->createToken($request->device_name)->plainTextToken;
-        return response()->json(['token' => $token]);
-    }
+        else{
 
+        }
+        //$token = $user->createToken($request->device_name)->plainTextToken;
+        return response()->json(['user' => $user]);
+    }
+    
     public function logout()
     {
         
